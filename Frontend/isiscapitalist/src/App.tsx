08189cca27f1966fb6b6f429ba3e1402e7222dc3
emattone <elsa.mattone@etud.univ-jfc.fr>
2023-02-15 17:50:16 +0100

    Front + Images

--- Frontend/isiscapitalist/src/App.tsx
+++ Frontend/isiscapitalist/src/App.tsx
@@ -1,19 +1,29 @@
-import React, { useState } from 'react';
+import React, { ChangeEvent, useEffect, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import { gql } from '@apollo/client';
+import { gql, useApolloClient, useQuery } from '@apollo/client';
 import Main from './Main';
 
-//const [username, setUsername] = useState("")
-//localStorage.setItem("username", username);
-
-/*function onUserNameChanged(){
-  let username = localStorage.getItem("username");
-
-  return username;
-}*/
-
 function App() {
+  const [username, setUsername] = useState("")
+  const client = useApolloClient();
+  useEffect(() => {
+    let first_username = localStorage.getItem("username")
+    if (!first_username) {
+      let x = Math.floor(Math.random() * 10000);
+      first_username = "Captain" + x;
+      localStorage.setItem("username", first_username)
+    }
+    setUsername(first_username)
+  },[])
+
+  function onUserNameChanged(event : ChangeEvent<HTMLInputElement>){
+    let new_userN = event.target.value
+    setUsername(new_userN)
+    localStorage.setItem("username", new_userN);
+    client.resetStore();
+  }
+
   const GET_WORLD = gql`
     query getWorld {
       getWorld {
@@ -85,9 +95,22 @@ function App() {
       }
   }`
 
+  const {loading, error, data, refetch } = useQuery(GET_WORLD, {
+    context: { headers: { "x-user": username } }
+  });
+
+  let corps = undefined
+  if (loading) corps = <div> Loading... </div>
+  else if (error) corps = <div> Erreur de chargement du monde ! </div>
+  else corps = <Main loadworld={data.getWorld} username={username} />
+
   return (
-    <><div><Main></Main></div>
-    <div><input /*className='Main' type="text" value={username} onChange={onUserNameChanged}*/ /></div></>
+    <div>
+    <div> Your ID :
+    <input type="text" value={username} onChange={onUserNameChanged}/>
+    { corps }
+    </div>
+    </div>
     
   );
 }
--- Frontend/isiscapitalist/src/Main.tsx
+++ Frontend/isiscapitalist/src/Main.tsx
@@ -1,27 +1,31 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { World } from './world';
 
-
-function Main() {
+type MainProps = {
+  loadworld: World
+  username: string
+}
+  export default function Main({ loadworld, username } : MainProps) {
+    const [world, setWorld] = useState(JSON.parse(JSON.stringify(loadworld)) as World)
+    useEffect(() => {
+      setWorld(JSON.parse(JSON.stringify(loadworld)) as World)
+     }, [loadworld])
     return (
-        <><div className="header">
-      <div> logo monde </div>
-      <div> argent </div>
-      <div> multiplicateur </div>
-      <div> ID du joueur </div>
-    </div><div className="main">
-        <div> liste des boutons de menu </div>
-        <div className="product">
-          <div> premier produit </div>
-          <div> second produit </div>
-          <div> troisième produit </div>
-          <div> quatrième produit </div>
-          <div> cinquième produit </div>
-          <div> sixième produit </div>
+        <div className="header">
+          <div>logo monde</div>
+          <div>argent</div>
+          <div>multiplicateur</div>
+        <div className="main" />
+          <div>liste des boutons de menu</div>
+        <div className="product"/>
+          <div>premier produit</div>
+          <div>second produit</div>
+          <div>troisième produit</div>
+          <div>quatrième produit</div>
+          <div>cinquième produit</div>
+          <div>sixième produit</div>
         </div>
-      </div></>
     );
 }
-
-export default Main;
