bd2f8c4b75288573b1c23246c6be983a29707716
Paulchaubet <paul.chaubet@etud.univ-jfc.fr>
2023-02-22 09:59:20 +0100

    Continuation du Backend

--- Backend/resolvers.js
+++ Backend/resolvers.js
@@ -15,11 +15,26 @@ function saveWorld(context) {
 
 function calcScore(context) {
     for (var p in context.world.products) {
-        if (p.timeleft != 0) {
-            if (context.world.lastupdate - p.timeleft < 0) {
-                context.world.score += p.cout
+        tempsEcoule = Date.now().toString - parseInt(context.world.lastupdate)
+        tempsEcouleInt = tempsEcoule -= p.timeleft
+        if (tempsEcoule < 0) {
+            p.timeleft = -tempsEcoule
+        } else [
+            p.timeleft = Math.floor((tempsEcoule) / p.vitesse)
+        ]
+        if (p.managerUnlocked == false) {
+            if (p.timeleft != 0) {
+                if (p.timeleft < tempsEcoule) {
+                    context.world.score += p.cout
+                } else {
+                    p.timeleft = p.timeleft - tempsEcouleInt
+                }
             } else {
-
+                if (p.timeleft < tempsEcoule) {
+                    context.world.score += p.cout
+                } else {
+                    p.timeleft = Math.floor((tempsEcoule) / p.vitesse)
+                }
             }
         }
     }
@@ -29,7 +44,7 @@ function calcScore(context) {
 module.exports = {
     Query: {
         getWorld(parent, args, context, info) {
-            calcScore()
+            calcScore(context)
             saveWorld(context)
             return context.world
         }
@@ -40,14 +55,11 @@ module.exports = {
             if (produit == undefined) {
                 throw new Error(`Le produit avec l'id ${args.id} n'existe pas`)
             } else {
-                tempsEcoule = context.world.lastupdate - Date.now()
-                if (tempsEcoule >= produit.timeleft) {
-                    calcScore()
-                    produit.quantite += args.quantite
-                    context.world.money -= produit.cout
-                    produit.cout = produit.cout * produit.croissance
-                    context.world.lastupdate = Date.now()
-                }
+                calcScore(context)
+                produit.quantite += args.quantite
+                context.world.money -= produit.cout
+                produit.cout = produit.cout * produit.croissance
+                context.world.lastupdate = Date.now().toString
             }
             return produit
         },
@@ -57,8 +69,9 @@ module.exports = {
             if (produit == undefined) {
                 throw new Error(`Le produit avec l'id ${args.id} n'existe pas`)
             } else {
+                calcScore(context)
                 produit.vitesse = produit.timeleft
-                context.world.lastupdate = Date.now()
+                context.world.lastupdate = Date.now().toString
             }
             return produit
         },
@@ -68,11 +81,14 @@ module.exports = {
             if (manager == undefined) {
                 throw new Error(`Le manager avec le nom ${args.name} n'existe pas`)
             } else {
+                calcScore(context)
                 let produitManager = context.world.products.find(p => p.id == manager.idcible)
                 manager.unlocked = !manager.unlocked
                 produitManager.managerUnlocked = !produitManager.managerUnlocked
+                context.world.lastupdate = Date.now().toString
             }
             return manager
         }
+
     }
 };
